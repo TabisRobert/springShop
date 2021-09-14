0278854040ab3faecb88e2a5d55c07dfca25e443
Robert Tabiś <robert.tabis@hotmail.com>
2021-09-15 00:15:00 +0200

    openHomePage method which opens home page in browser after launch of app

--- src/main/java/com/motorola/akademia/springShop/SpringShopApplication.java
+++ src/main/java/com/motorola/akademia/springShop/SpringShopApplication.java
@@ -3,11 +3,19 @@ package com.motorola.akademia.springShop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class SpringShopApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SpringShopApplication.class, args);
+		openHomePage();
+	}
+
+	private static void openHomePage() throws IOException {
+		Runtime rt = Runtime.getRuntime();
+		rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080/home");
 	}
 
 }
